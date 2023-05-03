c65c72b5ad254f68c24427e09a87d2c3d2dee009
Ahmad Qureshi <ahqu00001@stud.uni-saarland.de>
2023-05-03 21:55:27 +0200

    hello can you hear me

--- src/moduloStr.asm
+++ src/moduloStr.asm
@@ -11,4 +11,25 @@
 # v0: the decimal number (encoded using ASCII digits '0' to '9') in the buffer [$a0 to $a0 + $a1 - 1] modulo $a2 
 modulo_str:
 	# TODO
+	addi $t0, $a0, 0   # load address of the buffer
+	addi $t1,$0,0 #counter
+	move $t5,$a2  #we move divisor to t5
+	
+	addi $t3, $t3, 0 #our result hold
+loop_modulo:
+	lb $t2, 0($t0) #load first character into t2
+	addi $t1, $t1, 1       # increment counter LOOP Counter
+    	addi $t0, $t0, 1  	#increment address
+    	
+    	#here is modulo
+    	mul $t3,$t3,10
+    	addu $t3,$t3,$t2
+	remu $t3,$t3,$t5
+	
+	bne $t1, $a1, loop_modulo 
+	
+	
+	move $v0,$t3
+	
+	
 	jr	$ra
